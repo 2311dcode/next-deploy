02ea9fbda35d1fc0fecd71df2fc3da231d83ef05
phoebe7010 <phoebe7010@daum.net>
2024-01-15 10:55:40 +0900

    next14 init

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import clsx from 'clsx';
+import styles from './main.module.scss';
+
+export default function Home() {
+  return (
+    <main className={clsx(styles.main)}>
+      <h1>main page</h1>
+    </main>
+  );
+}
