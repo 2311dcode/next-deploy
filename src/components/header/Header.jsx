9263a9e430d9790c22fd8b8f1f9a7a76cc6a7b69
phoebe7010 <phoebe7010@daum.net>
2024-01-15 14:58:37 +0900

    dynamic import로 특정 클라이언트 컴포넌트 ssr에서 제외

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,12 @@ import clsx from 'clsx';
 import styles from './header.module.scss';
 import Link from 'next/link';
 import Navbar from '../navbar/Navbar';
+import dynamic from 'next/dynamic';
+
+//기존 client방식의 컴포넌트를  import시 ssr:false를 통해서 서버쪽에서 pre-build되지 않도록 설정
+const NoSsrNavibar = dynamic(() => import('@/components/navbar/Navbar'), {
+  ssr: false,
+});
 
 export default function Header() {
   return (
@@ -10,7 +16,8 @@ export default function Header() {
         <Link href="/">DCODELAB</Link>
       </h1>
 
-      <Navbar textArr={['about', 'youtube', 'post']} />
+      {/* <Navbar textArr={['about', 'youtube', 'post']} /> */}
+      <NoSsrNavibar textArr={['about', 'youtube', 'post']} />
     </header>
   );
 }
