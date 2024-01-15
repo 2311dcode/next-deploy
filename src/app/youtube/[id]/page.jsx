ec453021a61254529e0879ea41d1d253bfdce224
phoebe7010 <phoebe7010@daum.net>
2024-01-15 14:22:25 +0900

    navBar컴포넌트 분리

---
+++ src/app/youtube/[id]/page.jsx
@@ -0,0 +1,10 @@
+import clsx from 'clsx';
+import styles from './detail.module.scss';
+
+export default function Detail() {
+  return (
+    <section className={clsx(styles.detail)}>
+      <h1>detail</h1>
+    </section>
+  );
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import clsx from 'clsx';
+import styles from './header.module.scss';
+import Link from 'next/link';
+import Navbar from '../navbar/Navbar';
+
+export default function Header() {
+  return (
+    <header className={clsx(styles.header)}>
+      <h1>
+        <Link href="/">DCODELAB</Link>
+      </h1>
+
+      <Navbar textArr={['about', 'youtube', 'post']} />
+    </header>
+  );
+}
