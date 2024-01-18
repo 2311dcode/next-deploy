bf77c040bc3319c9c5330f2c6d12684443ceefb4
phoebe7010 <phoebe7010@daum.net>
2024-01-18 11:57:14 +0900

    Post스키마 userid항목 추가 및 기존 mongoDB에서 posts컬랙선 삭제

--- src/lib/models.js
+++ src/lib/models.js
@@ -5,6 +5,7 @@ const postSchema = new mongoose.Schema(
     title: { type: String, required: true },
     desc: { type: String, required: true },
     img: { type: String },
+    userid: { type: String },
   },
   { timestamps: true }
 );
