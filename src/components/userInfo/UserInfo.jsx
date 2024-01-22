42bce58e7e6b1dbbe55c2f367baf72e0daee8221
phoebe7010 <phoebe7010@daum.net>
2024-01-22 11:44:17 +0900

    uploadthing 라우터 함수파일, 라우터 생성

--- src/components/userInfo/UserInfo.jsx
+++ src/components/userInfo/UserInfo.jsx
@@ -6,11 +6,11 @@ import Image from 'next/image';
 export default async function UserInfo({ email }) {
   const user = await getUser(email);
   return (
-    <article className={clsx(styles.userInfo)}>
+    <div className={clsx(styles.userInfo)}>
       <p>{user?.email}</p>
       <div className={styles.pic}>
         <Image src={user?.img} width={50} height={50} alt={user?.email} />
       </div>
-    </article>
+    </div>
   );
 }
