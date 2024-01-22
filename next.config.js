60b090caec6c18df1391533057f509abf117fa0e
phoebe7010 <phoebe7010@daum.net>
2024-01-22 10:48:38 +0900

    NextAuth인증 처리 구조

--- next.config.js
+++ next.config.js
@@ -12,8 +12,10 @@ const nextConfig = {
       { protocol: 'https', hostname: 'i.ytimg.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'plus.unsplash.com' },
-      { protocol: 'https', hostname: 'pbs.twimg.com' },
       { protocol: 'https', hostname: 'live.staticflickr.com' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'pbs.twimg.com' },
     ],
   },
 };
