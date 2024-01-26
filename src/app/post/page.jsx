98102ca1dc92b47a3dd9a250ee5ddbc8111c5477
phoebe7010 <phoebe7010@daum.net>
2024-01-26 10:01:53 +0900

    처음 페이지 라우터 이동시 Suspense로 컨텐츠 로딩처리

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,37 +1,15 @@
 import clsx from 'clsx';
 import styles from './post.module.scss';
-import { getPostsPage } from '@/lib/actions';
-import Image from 'next/image';
-import Link from 'next/link';
-import Pagination from '@/components/pagination/Pagination';
-import { auth } from '@/lib/auth';
+import PostList from '@/components/postList/PostList';
+import { Suspense } from 'react';
 
 export default async function Post({ searchParams }) {
-  const session = await auth();
-  const page = searchParams?.page || 1;
-  const { total, posts, nums } = await getPostsPage(page);
-
   return (
     <section className={clsx(styles.post)}>
       <h1>Post</h1>
-      <Pagination total={total} nums={nums} />
-      <nav>
-        <Link href="/post/write">Write Post</Link>
-      </nav>
-
-      {posts.map((post) => (
-        <article key={post._id}>
-          <div className={clsx(styles.pic)}>
-            {post.img && (
-              <Image src={post.img} alt={post.title} priority fill />
-            )}
-          </div>
-          <h2>
-            <Link href={`/post/${post._id}`}>{post.title}</Link>
-          </h2>
-          <p>{post.desc}</p>
-        </article>
-      ))}
+      <Suspense fallback={<p>Loading...</p>}>
+        <PostList />
+      </Suspense>
     </section>
   );
 }
